8f52f6473d47c9d2eae8358c58df0250fbed5537
Rishabh <mailrishabh1403@gmail.com>
2017-08-05 13:00:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+/* global require */
+
+let request = require('request');
+let cheerio = require('cheerio');
+
+let url = `https://www.tripadvisor.in/Attractions-g187497-Activities-Barcelona_Catalonia.html`;
